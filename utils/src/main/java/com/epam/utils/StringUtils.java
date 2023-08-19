4408bfd6cc19e1dd366a13cb37c108e7fc446e0c
Giorgi Tsintsadze <giorgi.tsintsadze@scnadiweb.com>
2023-08-19 23:26:58 +0400

    update for test

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,9 +2,13 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+
         try {
-            double num = Double.parseDouble(str);
-            return num > 0;
+            double number = Double.parseDouble(str);
+            return number > 0;
         } catch (NumberFormatException e) {
             return false;
         }
